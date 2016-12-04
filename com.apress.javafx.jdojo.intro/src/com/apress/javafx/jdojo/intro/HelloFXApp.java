5ca51babfd7f9bf625a4a8a762a618c5ef69ef87
cguevara_aguilar <cguevara_aguilar@192.168.1.68>
2016-12-03 23:37:45 -0600

    Creación del proyecto

---
+++ com.apress.javafx.jdojo.intro/src/com/apress/javafx/jdojo/intro/HelloFXApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apress.javafx.jdojo.intro;
+
+/**
+ *
+ * @author cguevara_aguilar
+ */
+public class HelloFXApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
