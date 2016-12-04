b3c31e2b866b73e3cccbdfc559cfeb6842c5c936
cguevara_aguilar <cguevara_aguilar@192.168.1.68>
2016-12-03 23:58:12 -0600

    Prueba

--- com.apress.javafx.jdojo.intro/src/com/apress/javafx/jdojo/intro/HelloFXApp.java
+++ com.apress.javafx.jdojo.intro/src/com/apress/javafx/jdojo/intro/HelloFXApp.java
@@ -16,6 +16,8 @@ public class HelloFXApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println ("Hola");
     }
     
 }
